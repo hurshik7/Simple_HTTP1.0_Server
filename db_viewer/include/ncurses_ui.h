5c23e5591de1909c0b03acbd1e790c78486b04bb
hurshik7 <hurshik7@gmail.com>
2023-03-27 01:35:10 -0700

    move ncurses_ui.h into db_viewer

--- db_viewer/include/ncurses_ui.h
+++ db_viewer/include/ncurses_ui.h
@@ -7,12 +7,15 @@
 #include <stdbool.h>
 
 
+#define TITLE "HTTP Server DB Viewer (POST requests)"
+
+
 void print_menu(WINDOW* menu_win, int highlight);
 WINDOW* create_menu_window(void);
 int navigate_menu(WINDOW* menu_win);
 void print_title(int startx);
 void init_ncurses(void);
-void view_database_wrapper(void);
+void view_database_wrapper(const char* path);
 
 
 #endif //HTTP_SERVER_NCURSES_UI_H
