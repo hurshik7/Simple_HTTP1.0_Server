9506bce2bf43a112c668c3de58ee5b7f515f0f89
hurshik7 <hurshik7@gmail.com>
2023-03-27 22:27:27 -0700

    change position of include guard

--- include/my_ndbm.h
+++ include/my_ndbm.h
@@ -1,13 +1,15 @@
+#ifndef HTTP_SERVER_MY_NDBM_H
+#define HTTP_SERVER_MY_NDBM_H
+
+
 #include <stdio.h>
 #include <ndbm.h>
 #include <fcntl.h>
 #include <string.h>
 #include <time.h>
 
-#ifndef HTTP_SERVER_MY_NDBM_H
-#define HTTP_SERVER_MY_NDBM_H
 
-typedef struct PostReqData{
+typedef struct PostReqData {
     const char* client_ip_addr;
     time_t access_time;
     char* req_data;
@@ -19,3 +21,4 @@ int insert_post_request_data(DBM *db, post_req_data_t *post_req_data);
 
 
 #endif //HTTP_SERVER_MY_NDBM_H
+
